7a029fd5423c92346475663fec0d067dc9bb2654
arloor <admin@arloor.com>
2026-02-13 00:08:17 +0800

    删除多个 Dockerfile 和测试脚本

--- image.test.sh
+++
@@ -1,4 +0,0 @@
- cargo build -r --features aws_lc_rs,mimalloc --no-default-features
- podman build -f Dockerfile.test -t quay.io/arloor/hyperway:test . --net host
- podman login quay.io -u arloor 
- podman push quay.io/arloor/hyperway:test
